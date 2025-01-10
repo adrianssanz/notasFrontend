516cda6bb78643cfe4d7a64698447c387c7dd1a0
adrianssanz <sanzadrian@hotmail.es>
2025-01-10 17:17:50 +0000

    Añadidas rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './layouts/main/main.component';
+import { LoginComponent } from './components/login/login.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: WelcomeComponent
+      },
+  {
+    path: 'login',
+    component: LoginComponent
+      }
+    ]
+  }
+];
