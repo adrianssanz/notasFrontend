3a5d1da5e9b575e0ca9b9ceb48bc57444a2a92cb
adrianssanz <sanzadrian@hotmail.es>
2025-01-10 17:16:20 +0000

    Diseñado layout main

--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../../components/footer/footer.component";
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
