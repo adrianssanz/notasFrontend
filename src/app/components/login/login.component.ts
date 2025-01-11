cc051641d6e00d91e35124f24b40c044d4acd47a
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-11 20:28:23 +0100

    Añadido manejo de mensajes de error en el componente de inicio de sesión y redirección al dashboard tras inicio exitoso

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
-  imports: [ FormsModule],
+  imports: [ FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   providers: [AuthService]
@@ -12,16 +15,19 @@ import { FormsModule } from '@angular/forms';
 export class LoginComponent {
   username: string ='';
   password: string='';
+  errorMessage: string='';
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService, private router: Router){}
 
   login(){
     this.authService.login(this.username, this.password).subscribe({
       next: (response)=>{
-        console.log('Inicio de sesión exitoso', response);
+        this.router.navigate(['/dashboard']);
+        console.log(response.message);
       },
       error: (err) => {
-        console.error('Error al iniciar sesión', err);
+        console.error(err.error.message);
+        this.errorMessage = err.error.message;
       }
     });
   }
