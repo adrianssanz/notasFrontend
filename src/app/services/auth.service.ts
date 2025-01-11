9ed864f13a91697ece55201ae936d6aded34872b
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-11 20:27:33 +0100

    Añadido manejo de sesión y métodos de cierre de sesión en el servicio de autenticación

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  private baseUrl = 'http://localhost:8080/auth'; // Ajusta la URL base según tu API
+  private baseUrl = 'http://localhost:8080/auth';
 
   constructor(private http: HttpClient) {}
+  
 
   login(username: string, password: string): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    const body = { username, password };
-
+    const body = { usuario: username, password };
     return this.http.post(`${this.baseUrl}/login`, body, { headers, withCredentials: true });
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.http.get(`${this.baseUrl}/sesion`, {withCredentials: true}).pipe(
+      map(() => true),
+      catchError(() => of(false))
+    );
+  }
+
+  logout(): Observable<any>{
+    return this.http.post(`${this.baseUrl}/logout`,{}, {withCredentials: true}).pipe(
+      map(() => true),
+      catchError(() => of(false))
+    );
   }
 }
