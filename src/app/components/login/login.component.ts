f0c34a2450af5d5036ffaaf97c8705949b80a829
adrianssanz <sanzadrian@hotmail.es>
2025-01-11 12:03:03 +0000

    Configurado login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  providers: [AuthService]
 })
 export class LoginComponent {
+  username: string ='';
+  password: string='';
 
+  constructor(private authService: AuthService){}
+
+  login(){
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response)=>{
+        console.log('Inicio de sesión exitoso', response);
+      },
+      error: (err) => {
+        console.error('Error al iniciar sesión', err);
+      }
+    });
+  }
 }
