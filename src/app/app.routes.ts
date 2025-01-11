1ec928eb6c479b205fa6952a41ff41ded307cf14
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-11 20:28:04 +0100

    Añadido componente Dashboard y guardia de autenticación a las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './layouts/main/main.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent, } from './components/login/login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { authGuard } from './guards/auth.guard';
+
 
 export const routes: Routes = [
   {
@@ -12,10 +15,14 @@ export const routes: Routes = [
         path: '',
         component: WelcomeComponent
       },
-  {
-    path: 'login',
-    component: LoginComponent
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent, canActivate: [authGuard]
       }
     ]
-  }
+  },
 ];
