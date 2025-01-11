95f769134e716e9bdfd3aa13cd840c8faed36f8b
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-11 20:28:33 +0100

    Añadido manejo de autenticación en el componente de encabezado con opciones de inicio de sesión y cierre de sesión

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importar CommonModule
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [ RouterModule ],
+  standalone: true, // Define el componente como independiente
+  imports: [CommonModule, RouterModule], // Incluir CommonModule aquí
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  loggedIn: boolean = false;
 
+  constructor(private authService: AuthService, private router: Router,private cdRef: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.authService.isLoggedIn().subscribe((isLoggedIn) => {
+      this.loggedIn = isLoggedIn;
+      console.log('Is logged in:', isLoggedIn);
+      this.cdRef.detectChanges();
+    });
+  }
+
+  logOut() {
+    this.authService.logout().subscribe(() => {
+      this.router.navigate(['/']);
+      this.loggedIn = false;
+      console.log('Logged out');
+      this.cdRef.detectChanges();
+    });
+  }
 }
