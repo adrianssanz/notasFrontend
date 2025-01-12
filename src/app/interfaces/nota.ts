63ab05e7d57714de5fc38f1e123f515d8518421b
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-12 17:42:34 +0100

    Añadidas interfaces estado y usuario

--- src/app/interfaces/nota.ts
+++ src/app/interfaces/nota.ts
@@ -1,6 +1,20 @@
+
 export interface Nota {
     id: number;
     titulo: string;
     descripcion: string;
     fechaCreacion: Date;
+    estado: Estado;
+    usuario: Usuario;
 }
+
+export interface Estado{
+    id: number;
+    estado: string;
+}
+
+export interface Usuario {
+    id: number;
+    usuario: string;
+    email: string;
+ }
