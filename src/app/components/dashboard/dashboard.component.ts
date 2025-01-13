3bbf5d62da4e6d5b5267f1f530eeb12ba1498538
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-13 19:48:41 +0100

    Diseñado componente dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { ListaNotasComponent } from '../lista-notas/lista-notas.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ HeaderComponent, ListaNotasComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
