2b7977e952fa155e314b4f6c75bdc49be81f8514
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-13 19:49:36 +0100

    Diseñado componente nota para mostrar la nota, y poder eliminarla

--- src/app/components/nota/nota.component.ts
+++ src/app/components/nota/nota.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Nota } from '../../interfaces/nota';
 import { ApiService } from '../../services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nota',
@@ -12,14 +13,14 @@ import { ApiService } from '../../services/api.service';
 export class NotaComponent {
   @Input() nota!: Nota;
 
-  constructor(private apiService: ApiService){
+  constructor(private apiService: ApiService, private router: Router){
 
   }
 
  deleteNota(id: number): void {
   this.apiService.deleteNota(id).subscribe({
     next: (data) => {
-    console.log(data);
+      window.location.reload();
     },
     error: (error) => {
     console.error('Error al cargar las notas:', error);
