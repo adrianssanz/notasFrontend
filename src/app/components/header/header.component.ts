bb1aaa3d745e6795458fa11c302e92a64427be84
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-18 11:02:46 +0100

    Añadidos botones para navegar y respectivas validaciones de ruta y rol

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalCerrarSesionComponent } from '../modal-cerrar-sesion/modal-cerrar-sesion.component';
 import { ModalCrearNotaComponent } from '../modal-crear-nota/modal-crear-nota.component';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -14,8 +16,12 @@ import { ModalCrearNotaComponent } from '../modal-crear-nota/modal-crear-nota.co
 })
 export class HeaderComponent {
   constructor(
-    private matDialog: MatDialog
+    private matDialog: MatDialog,
+    private authService: AuthService,
+    private router: Router
   ) {}
+
+  rolUser!: number;
 
   abrirModalCerrarSesion() {
     this.matDialog.open(ModalCerrarSesionComponent);
@@ -23,5 +29,24 @@ export class HeaderComponent {
 
   abrirModalCrearNota() {
     this.matDialog.open(ModalCrearNotaComponent);
+  }
+
+  verificarRutaDashboard() {
+    return this.router.url === '/dashboard'; 
+  }
+
+  verificarRutaUsuarios() {
+    return this.router.url === '/usuarios'; 
+  }
+
+  cargarRolUsuario(): void {
+    this.authService.getUser().subscribe((user) => {
+      if (user) {
+        this.rolUser = user.rol.id;
+        console.log(this.rolUser)
+      } else {
+        console.log('No hay usuario logueado.');
+      }
+    });
   }
 }
